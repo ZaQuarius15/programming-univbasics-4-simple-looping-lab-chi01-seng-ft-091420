c25e893b39af55d6d58ed875d61b4ecbda741f85
Zack Carlson <jzcarlson014@gmail.com>
2020-08-18 22:53:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,30 @@
-# Write your methods here
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+  
+def loop_message_n_times(string, number)
+  int = number.to_i 
+  int.times do
+    puts string
+  end
+end  
+  
+def output_array(array)
+  puts array
+end
+  
+def return_string_array(array)
+  counter = 0
+  while array[counter] do
+    i.to_s
+  end
+end  
+  
+  
+  
+  
+  
+  
+  # Write your methods here
